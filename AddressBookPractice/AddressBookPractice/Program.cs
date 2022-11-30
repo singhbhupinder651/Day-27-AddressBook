e0dece0736d485cb83c7eb9bb26c52b05461ed04
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-11-30 19:40:03 +0530

    updated

---
+++ AddressBookPractice/AddressBookPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AddressBookPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            //UC-13
+            MultipleAddressBooks multipleAddressBooks = new MultipleAddressBooks();
+
+            //adding multiple address books
+            multipleAddressBooks.addAddressBook();
+            multipleAddressBooks.addAddressBook();
+
+            //adding contacts to above address books
+            multipleAddressBooks.addContact();
+            multipleAddressBooks.addContact();
+            multipleAddressBooks.addContact();
+            multipleAddressBooks.addContact();
+            multipleAddressBooks.addContact();
+
+            //sorting address books by name (alphabetically) before writing to text file
+            multipleAddressBooks.SortContactsInAddressBookByComparingFullName();
+
+            //writing to text file
+            multipleAddressBooks.WriteToTextFile();
+            
+            //reading from text file
+            MultipleAddressBooks.ReadFromTextFile();
+
+
+        }
+    }
+}
